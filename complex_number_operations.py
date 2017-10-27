7b0123788f0cfb36d92eac1b4e0ee73e0ac45642
Junjie Li <bioljj@yahoo.com>
2017-10-27 11:14:15 -0500

    Update file
    Add test class.

--- complex_number_operations.py
+++ complex_number_operations.py
@@ -42,3 +42,32 @@ def complex_num_negation(num):
     L = complex_to_number(num)
     
     return complex(-int(L[0]), -int(L[1]))
+
+'''Create test class'''
+import unittest
+
+class Test_Complex_Number_Operation(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(complex_num_addition(-2j, -1+3j), (-1+1j))
+        self.assertEqual(complex_num_addition(-2, -1+3j), (-3+3j))
+        self.assertEqual(complex_num_addition(6-2j, -1+3j), (5+1j))
+        self.assertEqual(complex_num_addition(8+4j, -1-3j), (7+1j))
+        self.assertEqual(complex_num_addition(0, 0), (0j))
+
+    def test_multiplication(self):
+        self.assertEqual(complex_num_multiplication(-2j, -1+3j), (6+2j))
+        self.assertEqual(complex_num_multiplication(-2, -1+3j), (2-6j))
+        self.assertEqual(complex_num_multiplication(6-2j, -1+3j), (20j))
+        self.assertEqual(complex_num_multiplication(4j, -1+3j), (-12-4j))
+        self.assertEqual(complex_num_multiplication(8+4j, -1-3j), (4-28j))
+
+    def test_negation(self):
+        self.assertEqual(complex_num_negation(-2j), (2j))
+        self.assertEqual(complex_num_negation(6-2j), (-6+2j))
+        self.assertEqual(complex_num_negation(8), (-8))
+        self.assertEqual(complex_num_negation(-8+6j), (8-6j))
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
